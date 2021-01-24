345d29032d3d2b60c96eb1f57fcb46e6f9c0158e
Krafi <jakub.havlicek5@gmail.com>
2021-01-24 17:20:45 +0100

    Passss

--- main.py
+++ main.py
@@ -123,6 +123,7 @@ def main():
         accel = sh.get_accelerometer_raw()
         magnitude = sqrt(accel["x"]**2 + accel["y"]**2 + accel["z"]**2)
         if magnitude > vib_treshold:
+            pass
             # sh.show_message(vib_message)
 
         # Update the current time
