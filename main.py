03841c48528f6ddec4bfa8a5b3d8d494ba7c659f
Krafi <jakub.havlicek5@gmail.com>
2021-01-24 16:49:26 +0100

    Sin goes to -1 you dummy

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def main():
         if delta_t(prev_d, now_time) > (1 / d_freq):
             print("AAAAAAA")
             prev_d = now_time
-            image = [[int(col * sin(time_k * (now_time.second + now_time.microsecond / 10**6))) for col in rgb] for rgb in flag]
+            image = [[int(col * (0.5 + 0.5 * sin(time_k * (now_time.second + now_time.microsecond / 10**6)))) for col in rgb] for rgb in flag]
             sh.set_pixels(image)
         
         # Take a measurment
