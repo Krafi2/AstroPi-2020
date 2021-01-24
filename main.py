9828e7fa7b888228a4fb4957a0d62aa25f11298e
Ekatka <60407802+Ekatka@users.noreply.github.com>
2021-01-24 13:09:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from pathlib import Path
 import csv
 
 
-
 def create_csv_file(data_file):
     """Create a new CSV file and add the header row"""
     with open(data_file, 'w') as f:
@@ -17,25 +16,28 @@ def create_csv_file(data_file):
         header = ("Date/time", "Magnetometer", "Gyroscope", "Accelerometer")
         writer.writerow(header)
 
+
 def add_csv_data(data_file, data):
     """Add a row of data to the data_file CSV"""
     with open(data_file, 'a') as f:
         writer = csv.writer(f)
         writer.writerow(data)
 
+
 def get_latlon():
     """Return the current latitude and longitude, in degrees"""
-    iss.compute() # Get the lat/long values from ephem
+    iss.compute()  # Get the lat/long values from ephem
     return (iss.sublat / degree, iss.sublong / degree)
 
 
 def main():
+    counter = 0
     dir_path = Path(__file__).parent.resolve()
 
     # Set a logfile name
-    logfile(dir_path/"kgang.log")
+    logfile(dir_path / "kgang.log")
 
-    data_file = dir_path/"data.csv"
+    data_file = dir_path / "data.csv"
 
     create_csv_file(data_file)
 
@@ -46,43 +48,52 @@ def main():
     line1 = "1 25544U 98067A   21013.52860115  .00001434  00000-0  33837-4 0  9995"
     line2 = "2 25544  51.6460  27.6964 0000416 223.1682 273.8476 15.49286819264623"
     iss = readtle(name, line1, line2)
-    
+
     red = [200, 0, 0]
     white = [200, 200, 200]
     blue = [0, 0, 200]
     black = [0, 0, 0]
     yellow = [200, 200, 0]
-    
+
     flag = [
-    blue, white, white, white, white, white, white, white,
-    blue, blue, white, white, white, white, white, white,
-    blue, blue, blue, white, white, white, white, white,
-    blue, blue, blue, blue, white, white, white, white,
-    blue, blue, blue, blue, red, red, red, red,
-    blue, blue, blue, red, red, red, red, red,
-    blue, blue, red, red, red, red, red, red,
-    blue, red, red, red, red, red, red, red
-    ]
-    
-    smile = [
-    yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow,
-    yellow, yellow, black, yellow, yellow, black, yellow, yellow,
-    yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow,
-    yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow,
-    yellow, black, yellow, yellow, yellow, yellow, black, yellow,
-    yellow, yellow, black, yellow, yellow, black, yellow, yellow,
-    yellow, yellow, yellow, black, black, yellow, yellow, yellow,
-    yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow
+        blue, white, white, white, white, white, white, white,
+        blue, blue, white, white, white, white, white, white,
+        blue, blue, blue, white, white, white, white, white,
+        blue, blue, blue, blue, white, white, white, white,
+        blue, blue, blue, blue, red, red, red, red,
+        blue, blue, blue, red, red, red, red, red,
+        blue, blue, red, red, red, red, red, red,
+        blue, red, red, red, red, red, red, red
     ]
 
+    smile = [
+        yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow,
+        yellow, yellow, black, yellow, yellow, black, yellow, yellow,
+        yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow,
+        yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow,
+        yellow, black, yellow, yellow, yellow, yellow, black, yellow,
+        yellow, yellow, black, yellow, yellow, black, yellow, yellow,
+        yellow, yellow, yellow, black, black, yellow, yellow, yellow,
+        yellow, yellow, yellow, yellow, yellow, yellow, yellow, yellow
+    ]
+    previous = smile
 
     while (now_time < start_time + timedelta(minutes=178)):
+        if counter == 50:
+            counter += 1
+            if previous == smile:
+                sh.set_pixels(flag)
+                previous = flag
+            else:
+                sh.set_pixels(smile)
+                previous = flag
+        else:
+            counter += 1
         try:
             magnetometer = sh.get_compass()
             accelerometer = sh.get_accelerometer()
             gyroscope = sh.get_gyroscope()
-    
-  
+
             # get latitude and longitude
             latitude, longitude = get_latlon()
             # Save the data to the file
@@ -99,6 +110,7 @@ def main():
             now_time = datetime.now()
         except Exception as e:
             logger.error('{}: {})'.format(e.__class__.__name__, e))
+
 
 if __name__ == "__main__":
     main()
